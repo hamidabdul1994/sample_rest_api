cd9b53bd6c18eaa0cc0f4dcb279011a5e82ec75e
Hamid Raza Noori <hamidabdul1994@hotmail.com>
2018-02-23 13:11:21 +0530

    router path defined

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,19 +1,19 @@
 var userService = require("../services").userService;
 
 module.exports = {
-    addEmployee:function(req,res){
+    addEmployee:function(req,res,next){
         
     },
-    updateEmployee : function(req,res){
+    updateEmployee : function(req,res,next){
 
     },
-    deleteEmployee : function(req,res){
+    deleteEmployee : function(req,res,next){
 
     },
-    readEmployee : function(req,res){
+    readEmployee : function(req,res,next){
 
     },
-    readAllEmployee: function(req,res){
+    readAllEmployee: function(req,res,next){
 
     }
 }
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,18 @@
+import { usersController } from '../controllers/index';
+
 var express = require('express');
 var router = express.Router();
 var upload = require('express-fileupload')
 var profileController = require("../controllers").pictureController;
+var userController = require("../controllers").usersController;
 
-/* GET */
-router.get('/', function(req, res, next) {
-  res.send({ title: 'Express' });
-});
+/* POST */
+router.post('/addEmployee', userController.addEmployee);
+router.delete("/deleteEmployee",userController.deleteEmployee);
+router.get("/readEmployee",usersController.readEmployee);
+router.get("/readAllEmployee",usersController.readAllEmployee);
+router.put("/updateEmployee",userController.updateEmployee);
+
 router.post("/uploadPicture",upload(),profileController.uploadPicture);
+
 module.exports = router;
