93a354aa0510e3fe0fbf149fb74abf4fb09e249d
Hamid Raza Noori <hamidabdul1994@hotmail.com>
2018-02-23 13:14:03 +0530

    name issue resolved

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,3 @@
-import { usersController } from '../controllers/index';
-
 var express = require('express');
 var router = express.Router();
 var upload = require('express-fileupload')
@@ -9,8 +7,8 @@ var userController = require("../controllers").usersController;
 /* POST */
 router.post('/addEmployee', userController.addEmployee);
 router.delete("/deleteEmployee",userController.deleteEmployee);
-router.get("/readEmployee",usersController.readEmployee);
-router.get("/readAllEmployee",usersController.readAllEmployee);
+router.get("/readEmployee",userController.readEmployee);
+router.get("/readAllEmployee",userController.readAllEmployee);
 router.put("/updateEmployee",userController.updateEmployee);
 
 router.post("/uploadPicture",upload(),profileController.uploadPicture);
