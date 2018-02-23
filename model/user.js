577413513858744a9c864d13cd232c3343a36d06
Hamid Raza Noori <hamidabdul1994@hotmail.com>
2018-02-23 17:43:02 +0530

    Model Static function done

--- model/user.js
+++ model/user.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose'),
 var BaseModel = require("./base");
 
 
-const UserSchema =BaseModel.extend({ 
+const UserSchema = BaseModel.extend({ 
         firstName: String, 
         lastName: String, 
         emailAddress : {
@@ -19,6 +19,53 @@ const UserSchema =BaseModel.extend({
         }
     });
     
+    UserSchema.set('toJSON', {
+        virtuals: true,
+        transform: function(doc, ret, field) {
+            ret.userId = ret._id;
+            delete ret._id;
+            delete ret.__v;
+            return ret;
+        }
+    });
+    /**
+     * @description To save user
+     **/
+    UserSchema.statics.saveUser = function(userObj,callback){
+        var userObject = new this(userObj);
+        userObject.save(callback);
+    };
+
+    /****
+     * @description read Employee Details emailAddress
+     ***/
+    UserSchema.statics.readUser = function(emailAddress,callback){
+        this.findOne({emailAddress : emailAddress , isDeleted : false},callback);
+    };
+
+    /****
+     * @description to update Employee Details by emailAddress
+     ***/
+    UserSchema.statics.readUser = function(userObj,callback){
+        if(userObj.emailAddress){
+            return callback("emailAddress is required for update");
+        }
+        this.update({emailAddress : userObj.emailAddress , isDeleted : false},
+            { $set: userObj },callback);
+    };
+
+
+    /****
+     * @description to remove Employee Details by emailAddress
+     ***/
+    UserSchema.statics.removeUser = function(emailAddress,callback){
+        if(emailAddress){
+            return callback("emailAddress is required for update");
+        }
+        this.update({ emailAddress : emailAddress , isDeleted : false },
+            {$set : { isDeleted : true }},callback);
+    };
+    // UserSchema.methods.
 var UserModel = mongoose.model('User', UserSchema);
 
 module.exports = UserModel; 
